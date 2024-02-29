020bb3a28c64110a14d22a15ff4ebbdfc0a212ea
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-02-29 11:40:22 -0600

    add Die container

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import Die from "./components/Die"
 
 export default function App() {
     return (
         <main>
-          
+          <div className="dice-container">
+            
+          </div>
         </main>
     )
 }
