bf27ccd3341d19d151ad2f8befe1c5488f410b70
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2024-02-29 12:52:33 -0600

    add functionality to generate randomized Dice components

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -2,10 +2,24 @@ import React from "react"
 import Die from "./components/Die"
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = [];
+    for(let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map(die => <Die value={die}/>)
+
+  console.log(allNewDice(1, 6))
+
     return (
         <main>
           <div className="dice-container">
-            
+            {diceElements}
           </div>
         </main>
     )
